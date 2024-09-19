c43d3d86eb7fa637a9e39705b40990e2a86c7162
sindlinger <candeia.goncalves@gmail.com>
2024-09-19 01:05:27 -0300

    depois de add o repo

---
+++ prisma/migrations/20240919023953_init/migration.sql
@@ -0,0 +1,4 @@
+-- CreateTable
+CREATE TABLE "Foo" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT
+);
--- remix.config.js
+++ remix.config.js
@@ -2,11 +2,13 @@
 module.exports = {
   cacheDirectory: "./node_modules/.cache/remix",
   future: {
-    v2_errorBoundary: true,
-    v2_meta: true,
-    v2_normalizeFormMethod: true,
-    v2_routeConvention: true,
+    v2_errorBoundary: true,  // Ativa a nova API de gerenciamento de erros
+    v2_meta: true,           // Ativa o novo formato para meta tags
+    v2_normalizeFormMethod: true,  // Padroniza os métodos de formulário para letras maiúsculas
+    v2_routeConvention: true,  // Usa a nova convenção de rotas
+    v2_headers: true,  // Ativa o novo comportamento de cabeçalhos nas rotas
   },
   ignoredRouteFiles: ["**/.*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
-  serverDependenciesToBundle: ["normalize-url"]
+  serverDependenciesToBundle: ["normalize-url"],
+  serverModuleFormat: 'esm',  // Muda o formato do módulo do servidor para ESM
 };
